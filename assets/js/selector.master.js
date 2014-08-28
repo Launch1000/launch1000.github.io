237aa721c1c5ac0bcd2a44a8c80cfcbaa7936bfe
Kames Cox-Geraghty <kamesg@gmail.com>
2014-08-28 01:04:45 -0700

    Commit

--- assets/js/selector.master.js
+++ assets/js/selector.master.js
@@ -1,45 +1,12 @@
  $(document).ready(function() {
 //BEGIN jQuery Selectors
-	//BEGIN Smoove
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 10) {
-            $('nav').addClass('shrink-nav');
-        } else {
-            $('nav').removeClass('shrink-nav');
-        }
-    });
-jQuery(function($) {
-                $(document).ready( function() {
-                  //enabling stickUp on the '.navbar-wrapper' class
 
-                  $('.navigation').stickUp({ marginTop: 'auto',
-                        parts: {
-                          0:'solutions-section',
-                          1:'design_development-section',
-                          2:'email_communication-section',
-                          3:'hosting_domain-section',
-                          4:'start_new_project-section',
-                        },
-                        itemClass: 'menuItem',
-                        itemHover: 'active'
-                      });
-                });
-              });
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+  var hero = document.getElementById('hero');
+  var parallax = new Parallax(hero);
+  var utility = document.getElementById('utility');
+  var parallax = new Parallax(utility);
+  var action = document.getElementById('action');
+  var parallax = new Parallax(action);
 
 //END jQuery Selectors    
 });
