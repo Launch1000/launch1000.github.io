7d81367987057444ce64fa7672c678ec4614292c
Kames Cox-Geraghty <kamesg@gmail.com>
2014-08-28 10:35:41 -0700

    Update

--- assets/js/selector.master.js
+++ assets/js/selector.master.js
@@ -1,12 +1,78 @@
  $(document).ready(function() {
 //BEGIN jQuery Selectors
+	//BEGIN Smoove
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 0) {
+            $('nav#index').addClass('scroll-down');
+        } else {
+            $('nav#index').removeClass('scroll-down');
+        }
+    });
 
-  var hero = document.getElementById('hero');
-  var parallax = new Parallax(hero);
-  var utility = document.getElementById('utility');
-  var parallax = new Parallax(utility);
-  var action = document.getElementById('action');
-  var parallax = new Parallax(action);
+
+$('.covervid-video').coverVid(720, 480);
+
+    $('#main').scrollupba();
+  
+
+
+jQuery(function($) {
+                $(document).ready( function() {
+                  //enabling stickUp on the '.navbar-wrapper' class
+
+                  $('nav#inde').stickUp({ marginTop: 'auto',
+                        parts: {
+                          0:'solutions-section',
+                          1:'design_development-section',
+                          2:'email_communication-section',
+                          3:'hosting_domain-section',
+                          4:'start_new_project-section',
+                        },
+                        itemClass: 'menuItem',
+                        itemHover: 'active'
+                      });
+                });
+              });
+
+
+jQuery(document).ready(function($){
+  var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+
+  //open team-member bio
+  $('#sider').find(' .action').on('click', function(event){
+    event.preventDefault();
+    var selected_member = $(this).data('type');
+    $('.slideInContainer.'+selected_member+'').addClass('slide-in');
+    $('.cd-member-bio-close').addClass('is-visible');
+
+    // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+    if( is_firefox ) {
+      $('body').addClass('slide-out').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        $('body').addClass('overflow-hidden');
+      });
+    } else {
+      $('body').addClass('slide-out');
+      $('body').addClass('overflow-hidden');
+    }
+
+  });
+
+  //close team-member bio
+  $(document).on('click', '.cd-overlay, .slideInContainer, .cd-member-bio-close', function(event){
+    event.preventDefault();
+    $('.slideInContainer').removeClass('slide-in');
+    $('.cd-member-bio-close').removeClass('is-visible');
+
+    if( is_firefox ) {
+      $('body').removeClass('slide-out').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        $('body').removeClass('overflow-hidden');
+      });
+    } else {
+      $('body').removeClass('slide-out');
+      $('body').removeClass('overflow-hidden');
+    }
+  });
+});
 
 //END jQuery Selectors    
 });
